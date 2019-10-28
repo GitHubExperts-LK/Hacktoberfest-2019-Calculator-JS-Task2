db96f0c4181d9b387a6d7e538ddcb231a86b7a9c
MalshaNavodi <malsha1310weerasinghe@gmail.com>
2019-10-28 14:53:59 +0530

    modified

--- task.js
+++ task.js
@@ -8,9 +8,9 @@ const calculator = {
 //Manage Operators
 const performCalculation = {
     '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
     '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
     '=': (firstOperand, secondOperand) => secondOperand
 };
 
